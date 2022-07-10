0609de6eb85e1efd3072495dc8b66d12ce9ea3b1
Maxim Ivanov (github username: maximivanov1711) <maxim.ivanov1711@gmail.com>
2022-07-10 15:55:11 +0300

    Refactored ConnectionManager class

--- server/main.py
+++ server/main.py
@@ -21,25 +21,25 @@ class ConnectionManager():
 
         if len(self.active_connections) == 1:
             await websocket.send_json({
-                'init': True,
-                'player': 'x',
-                'info': 'Waiting for second player.'
+                'type': 'connection',
+                'player': 'x'
             })
         elif len(self.active_connections) == 2:
             await websocket.send_json({
-                'init': True,
-                'player': 'o',
-                'info': ''
+                'type': 'connection',
+                'player': 'o'
             })
 
             await self.active_connections[0].send_json({
-                'init': True,
-                'player': 'x',
-                'info': 'Your turn.'
+                'type': 'start'
             })
     
-    def disconnect(self, websocket: WebSocket) -> None:
+    async def disconnect(self, websocket: WebSocket) -> None:
         self.active_connections.remove(websocket)
+
+        await self.active_connections[0].send_json({
+            'type': 'disconnect'
+        })
     
     async def send_personal_message(self, message: str, websocket: WebSocket) -> None:
         await websocket.send_text(message)
@@ -58,9 +58,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"User {client_id} says: {data}")
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            # await manager.broadcast(f"User {client_id} says: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"User {client_id} disconnected")
+        await manager.disconnect(websocket)
 
