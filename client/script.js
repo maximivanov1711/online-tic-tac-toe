0c8a1c2c4f79e7b93d12a90b690e0aa1f0e57a2b
Maxim Ivanov (github username: maximivanov1711) <maxim.ivanov1711@gmail.com>
2022-07-15 19:55:19 +0300

    Added ability to make a move

--- client/script.js
+++ client/script.js
@@ -1,16 +1,69 @@
+function debug(obj) {
+    if (DEBUG) {
+        console.log(obj);
+    }
+}
+
+const DEBUG = true;
+
 const client_id = Math.floor(Math.random() * Math.pow(10, 10))
 console.log(`You are User ${client_id}`);
 
 const ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
 
+let client_player;
+
+function setPlayer(player) {
+    const playerRow = document.getElementById('player');
+    playerRow.innerText = player;
+}
+
+function setInfo(infoText) {
+    const infoRow = document.getElementById("info");
+    infoRow.innerText = infoText;
+}
+
 function cellClick(cellId) {
-    ws.send(`Cell ${cellId} was clicked`);
+    ws.send(JSON.stringify({
+        'player': client_player,
+        'cell': cellId
+    }));
+}
+
+function updateCell(cellId, player) {
+    const cell = document.getElementById(cellId);
+    cell.innerText = player;
 }
 
 ws.onmessage = function(e) {
-    console.log(e);
+    debug(e);
+
+    const data = JSON.parse(e.data);
+    
+    if (data.type == 'connection') {
+        client_player = data.player;
+        setPlayer(client_player);
+        
+        if (client_player == 'x') {
+            setInfo('Waiting for another player.');
+        } else {
+            setInfo('Opponent turn.');
+        }
+    } else if (data.type == 'start') {
+        setInfo('Your turn.'); 
+    } else if (data.type == 'turn') {
+        updateCell(data.cell, data.player);
+    } else if (data.type == 'win') {
+
+    } else if (data.type == 'draw') {
+
+    } else if (data.type == 'denied' && data.player == client_player) {
+        setInfo(data.message);
+    }
 }
 
 ws.onclose = function(e) {
-    console.log(e);
+    if (e.code == 4000) {
+        setInfo('Game is already started.')
+    }
 }
--- server/main.py
+++ server/main.py
@@ -3,19 +3,29 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from typing import List
 
 
+def debug(obj):
+    global DEBUG
+    if DEBUG:
+        print(obj)
+
+
+DEBUG = True
+
+
 app = FastAPI()
 
 
 class ConnectionManager():
-    def __init__(self) -> None:
+    def __init__(self):
             self.active_connections: List[WebSocket] = []
     
-    async def connect(self, websocket: WebSocket) -> None:
+    async def connect(self, websocket: WebSocket):
         await websocket.accept()
 
         if len(self.active_connections) == 2:
             await websocket.close(4000)
-            return
+
+            return False
         
         self.active_connections.append(websocket)
 
@@ -24,6 +34,9 @@ class ConnectionManager():
                 'type': 'connection',
                 'player': 'x'
             })
+
+            self.connected_player = 'x'
+            return True
         elif len(self.active_connections) == 2:
             await websocket.send_json({
                 'type': 'connection',
@@ -33,33 +46,94 @@ class ConnectionManager():
             await self.active_connections[0].send_json({
                 'type': 'start'
             })
+
+            self.connected_player = 'o'
+            return True
     
-    async def disconnect(self, websocket: WebSocket) -> None:
+    async def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-        await self.active_connections[0].send_json({
-            'type': 'disconnect'
-        })
-    
-    async def send_personal_message(self, message: str, websocket: WebSocket) -> None:
-        await websocket.send_text(message)
+        if self.active_connections:
+            await self.active_connections[0].send_json({
+                'type': 'disconnect'
+            })
 
-    async def broadcast(self, message: str) -> None:
+    async def broadcast(self, message: dict):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_json(message)
+
+
+class GameManager():
+    def __init__(self):
+        self.board = [
+            ['*', '*', '*'], 
+            ['*', '*', '*'],
+            ['*', '*', '*']
+        ]
+
+        self.players = {}
+
+        self.is_game_started = False
+    
+    def add_player(self, player: str, websocket: WebSocket):
+        self.players[player] = websocket
+    
+    def start(self):
+        self.is_game_started = True
+        self.current_player = 'x'
+    
+    async def process_turn(self, turn: dict):
+        debug(turn)
+
+        if not self.is_game_started:
+            return
+
+        player = turn['player']
+        cell = turn['cell']
+
+        if self.current_player != player:
+            await manager.broadcast({
+                'type': 'denied',
+                'player': player,
+                'message': 'Wait for your turn'
+            })
+
+            return
+        
+        await manager.broadcast({
+                'type': 'turn',
+                'player': player,
+                'cell': cell
+            })
+
+
+            
 
 
 manager = ConnectionManager()
+game = GameManager()
 
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+    is_player_connected = await manager.connect(websocket)
+    
+    if not is_player_connected:
+        return
+    
+    connected_player = manager.connected_player
+
+    game.add_player(connected_player, websocket)
+    
+    if connected_player == 'o':
+        game.start()
+
     try:
         while True:
-            data = await websocket.receive_text()
-            # await manager.send_personal_message(f"You wrote: {data}", websocket)
-            # await manager.broadcast(f"User {client_id} says: {data}")
+            turn = await websocket.receive_json()
+            
+            await game.process_turn(turn)
+            
     except WebSocketDisconnect:
         await manager.disconnect(websocket)
 
